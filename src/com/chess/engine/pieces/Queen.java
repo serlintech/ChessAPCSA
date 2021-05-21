0880a22ec9841f5a15941913c5f8713855696408
Serlintech <serlintech@gmail.com>
2021-05-21 08:31:11 -0400

    New commit, Updates included to BoardUtils, Move, and Knight. Knight fixed to use correct variable names and work correctly. Move fleshed out as a new class, with two subclasses, an attack move or standard MajorMove.

--- src/com/chess/engine/pieces/Queen.java
+++ src/com/chess/engine/pieces/Queen.java
@@ -13,6 +13,10 @@ import java.util.List;
 public class Queen extends Piece{
     private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-9,-8, -7,-1,1, 7,8, 9};
 
+    Queen(int piecePos, Alliance pieceAlliance) {
+        super(piecePos, pieceAlliance);
+    }
+
     @Override
     public Collection<Move> legalMoves(Board board){
         final List<Move> legalMoves = new ArrayList<>();
