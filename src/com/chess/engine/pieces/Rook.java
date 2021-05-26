02b72c60696c7f554eff138db4e6f718bc4526ba
zimbabwe69420 <jamesondamico9@gmail.com>
2021-05-26 07:45:49 -0400

    ROOK CLASS arraylist containing possible coordinates, method to detect legal moves, and boolean methods specifiying if a 1st or 8th column exclusion is true or not. Derived from the bishop class

---
+++ src/com/chess/engine/pieces/Rook.java
@@ -0,0 +1,59 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Rook extends Piece{
+    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-8, -1, 1, 8};
+
+    Rook(int piecePosition, Alliance pieceAlliance){
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> legalMoves(Board board){
+        final List<Move> legalMoves = new ArrayList<>();
+        for(final int candidateCoordinateOffset: CANDIDATE_MOVE_VECTOR_COORDINATES){
+
+            int moveDestCoor = this.piecePos;
+
+            while(BoardUtils.isValidTileCoor(moveDestCoor)){
+                if(isFirstColumnExclusion(moveDestCoor, candidateCoordinateOffset) || isEightColumnExclusion(moveDestCoor,candidateCoordinateOffset)) {
+                    break;
+                }
+                moveDestCoor += candidateCoordinateOffset;
+                if (BoardUtils.isValidTileCoor(moveDestCoor)){
+                    final Tile moveDestTile = board.getTile(moveDestCoor);
+                    if (!moveDestTile.isTileOccupied()) {
+                        legalMoves.add(new Move.MajorMove(board, this, moveDestCoor));
+                    } else {
+                        final Piece pieceAtDest = moveDestTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDest.getPieceAlliance();
+                        if (this.pieceAlliance != pieceAlliance) {
+                            legalMoves.add(new Move.OffensiveMove(board,this,moveDestCoor, pieceAtDest));
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.FIRST_COLUMN(currentPosition) && (candidateOffset == -1);
+    }
+
+    private static boolean isEightColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.EIGTH_COLUMN(currentPosition) && (candidateOffset == 1);
+    }
+}
